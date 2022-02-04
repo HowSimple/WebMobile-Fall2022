d5adda06aae02eceb53cd8cf7044c66e86b0403f
Caleb Vaughn <43933950+HowSimple@users.noreply.github.com>
2022-02-03 18:46:39 -0600

    Rock paper scissors script handles all cases properly, and updates the webpage

--- Web-ICP3/Source/Rock Paper Scissors/rockpaperscissors.js
+++ Web-ICP3/Source/Rock Paper Scissors/rockpaperscissors.js
@@ -5,14 +5,31 @@ function playRockPaperScissors(selection){
     var randomMove = options[randomIndex]
 
     // change html page to show the player's selected move, and the computer's move
+
     document.getElementById("PlayerDecision").innerHTML = selection
     document.getElementById("ComputerDecision").innerHTML = randomMove
-
+    document.getElementById("results").style.visibility = "visible";
 
 
     if (selection == "Rock" && randomMove =="Scissors")
-    {   // rock breaks scissors
-        document.getElementById("results").innerHTML = "You win!"
+    {   // rock breaks scissors rule
+        document.getElementById("winner").innerHTML = "You win!"
+    }
+    else if (selection == "Scissors" && randomMove =="Paper")
+    {   // scissors cuts paper rule
+        document.getElementById("winner").innerHTML = "You win!"
+    }
+    else if (selection == "Paper" && randomMove =="Rock")
+    {   // paper covers rock rule
+        document.getElementById("winner").innerHTML = "You win!"
+    }
+    else if (selection == randomMove)
+    {   // tie game
+        document.getElementById("winner").innerHTML = "It's a draw!"
+    }
+    else
+    {
+        document.getElementById("winner").innerHTML = "You lose!"
     }
 
 
