e5a0e2a8f00aba190a91cf7ad68a77e296c5e446
Caleb Vaughn <43933950+HowSimple@users.noreply.github.com>
2022-02-08 12:45:34 -0600

    Update github.js

--- Web-ICP4/Source/GitHubUserFinder/github.js
+++ Web-ICP4/Source/GitHubUserFinder/github.js
@@ -10,6 +10,21 @@ function lookupAccount(){
     var json = request.responseText;
     var data = JSON.parse(request.responseText);
 
+    showAccountDetails(data)
+
     return data
 }
 
+function showAccountDetails(accountData)
+{
+    
+    document.getElementById("accountLogin").innerHTML = accountData.login
+    document.getElementById("accountName").innerHTML = accountData.name
+    document.getElementById("accountId").innerHTML = accountData.id
+    document.getElementById("accountLink").innerHTML = accountData.html_url
+    document.getElementById("accountRepositoryCount").innerHTML = accountData.public_repos
+
+    document.getElementById("accountAvatar").innerHTML = accountData.avatar_url
+
+
+}
