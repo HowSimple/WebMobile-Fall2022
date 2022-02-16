a0645ffcaee8f5379ddc2884076cb78d9d5d1f91
Caleb Vaughn <43933950+HowSimple@users.noreply.github.com>
2022-02-16 17:10:53 -0600

    Added component

--- Web-ICP5/src/app/app.module.ts
+++ Web-ICP5/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CountdownComponent } from './countdown/countdown.component';
 import { MemorygameComponent } from './memorygame/memorygame.component';
+import { CountdownTimerComponent } from './countdown-timer/countdown-timer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CountdownComponent,
-    MemorygameComponent
+    MemorygameComponent,
+    CountdownTimerComponent
   ],
   imports: [
     BrowserModule,
