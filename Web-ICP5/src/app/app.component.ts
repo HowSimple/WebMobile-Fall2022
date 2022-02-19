e3f4da6b9e0efa16b1e72979aa7bef9034482157
Caleb Vaughn <43933950+HowSimple@users.noreply.github.com>
2022-02-19 13:28:34 -0600

    Removed unused components

--- Web-ICP5/src/app/app.component.ts
+++ Web-ICP5/src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { CountdownComponent} from "./countdown/countdown.component";
 
 @Component({
   selector: 'app-root',
--- Web-ICP5/src/app/app.module.ts
+++ Web-ICP5/src/app/app.module.ts
@@ -3,7 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CountdownComponent } from './countdown/countdown.component';
 import { MemorygameComponent } from './memorygame/memorygame.component';
 import { CountdownTimerComponent } from './countdown-timer/countdown-timer.component';
 import { TodoListComponent } from './todo-list/todo-list.component';
@@ -13,7 +12,7 @@ import {FormsModule} from "@angular/forms";
 @NgModule({
   declarations: [
     AppComponent,
-    CountdownComponent,
+
     MemorygameComponent,
     CountdownTimerComponent,
     TodoListComponent,
