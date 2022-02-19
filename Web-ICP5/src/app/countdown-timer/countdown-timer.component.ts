2a4935a5530a5d1ba0223d50b5b8180855f6a7a0
Caleb Vaughn <43933950+HowSimple@users.noreply.github.com>
2022-02-19 12:38:56 -0600

    Removed unimplemented stop countdown button

--- Web-ICP5/src/app/countdown-timer/countdown-timer.component.ts
+++ Web-ICP5/src/app/countdown-timer/countdown-timer.component.ts
@@ -14,15 +14,16 @@ import {  Subscription, interval } from 'rxjs';
 export class CountdownTimerComponent implements OnInit{
 
   public startTime = new Date();
-  public eventTime = new Date('Jan 01 2025 00:00:00');
+  //public eventTime = new Date('Jan 01 2025 00:00:00');
+  public eventTime = new Date();
+  public countdownStarted!: boolean;
   public daysRemaining!: number;
   public hoursRemaining!: number;
   public minutesRemaining!: number;
   public secondsRemaining!: number;
   public millisecondsRemaining!: number;
-  public eventMonth!:number;
-  public eventDay!:number
-  public eventYear!:number;
+
+
   public timer:any;
 
 
@@ -74,6 +75,7 @@ export class CountdownTimerComponent implements OnInit{
    //public startTimer(month: number, day:number, year:number) {
   //console.log(dateInput.Year);
   //var year = document.getElementById("Year")!.value;
+
     var year =  (<HTMLInputElement>document.getElementById("Year")).value;
     var month =  (<HTMLInputElement>document.getElementById("Month")).value;
     var day =    (<HTMLInputElement>document.getElementById("Day")).value;
@@ -81,11 +83,10 @@ export class CountdownTimerComponent implements OnInit{
     this.startTime = new Date();
     this.eventTime = new Date(`${month}/${day}/${year}`)
     console.log(`${month}/${day}/${year}`)
-    //console.log(month.toString()+day.toString())
-
-
 
     this.updateTime();
+    this.countdownStarted = true;
+
     this.timer = setInterval(() => this.updateTime(),1000)
 
 
@@ -93,9 +94,6 @@ export class CountdownTimerComponent implements OnInit{
     this.updateTime()
     interval(1000).pipe(take(this.millisecondsRemaining), map(count => this.millisecondsRemaining - count)).subscribe(seconds => {
       this.updateTime();
-      //this.duration = 1000
-
-      //this.subscription = interval(this.duration).subscribe(x => this.timeDifference())
     })
 
 
@@ -107,7 +105,7 @@ export class CountdownTimerComponent implements OnInit{
     })}
 
   ngOnInit(): void {
-
+    this.countdownStarted = false;
     //const duration = 15 * 60 // 15 minutes
 
     //interval(1000).pipe(take(this.), map(count => duration - count)).subscribe(seconds => {
