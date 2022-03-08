7e12bc6e5eeb7062434a2015d17017f9a776ac20
Caleb Vaughn <43933950+HowSimple@users.noreply.github.com>
2022-03-07 18:36:39 -0600

    Connect to db

--- Web-ICP7/app.js
+++ Web-ICP7/app.js
@@ -1,7 +1,19 @@
 const express = require('express')
+const database = require('mongodb').MongoClient
+const mongoose = require('mongoose')
+
 const app = express()
 const port = 4001
 
+database.connect('mongodb://localhost:27017/test', (err,client) => {
+    if (err) throw err
+    const db = client.db('test')
+    db.collection('employees').find().toArray((err, result) => {
+        if (err) throw err
+
+        console.log(result)
+    })
+})
 
 app.get('/', (req, res) => {
     res.send("Hello World!")
