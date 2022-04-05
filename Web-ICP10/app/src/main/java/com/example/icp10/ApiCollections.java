fa7d59f2ae426c7c4c5849c6604eb1a4fc8302f0
Liz-Bai <xilo000628@gmail.com>
2022-04-04 23:13:57 -0500

    icp10

--- Web-ICP10/app/src/main/java/com/example/icp10/ApiCollections.java
+++ Web-ICP10/app/src/main/java/com/example/icp10/ApiCollections.java
@@ -1,14 +1,14 @@
 package com.example.icp10;
 
 import retrofit2.Call;
+import retrofit2.http.GET;
 
 import java.util.List;
 
 public interface ApiCollections {
 
     @GET("users")
+    Call<List<User>> getData();
 
-    Call<list<User>> getData();
-    
 
 }
